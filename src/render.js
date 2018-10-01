af980ad11464b5891f718b8c94a1ea34ffa5cfa0
Fabian Zickgraf <f.zickgraf@dashdos.com>
2018-10-01 11:30:12 +0200

    Add vertical alignment to diagram renderer
    This is optional and off by default.

--- src/render.js
+++ src/render.js
@@ -19,7 +19,7 @@ function resolveComponents(vnode) {
     }
 }
 
-export function renderToDiagram(vnode, co = false) {
+export function renderToDiagram(vnode, options = {}, co = false) {
     let diagramNode = resolveComponents(vnode)
 
     if (diagramNode == null || diagramNode.type !== Diagram)
@@ -28,14 +28,15 @@ export function renderToDiagram(vnode, co = false) {
     return new Diagram({
         ...diagramNode.props,
         co: co !== !!diagramNode.props.co,
+        align: options.align ? options.align : false,
         children: diagramNode.children
     })
 }
 
-export function render(vnode, co = false) {
-    return renderToDiagram(vnode, co).render()
+export function render(vnode, options = {}, co = false) {
+    return renderToDiagram(vnode, options, co).render()
 }
 
-export function corender(vnode) {
-    return render(vnode, true)
+export function corender(vnode, options = {}) {
+    return render(vnode, options, true)
 }
